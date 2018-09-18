88d3bfb63638ecd9e3fa59ecc6b6980bbb60b2b8
Lawrence Neal <nealla@lwneal.com>
2018-09-18 09:15:53 -0700

    Remove all the old...

--- sc2util/random_agent.py
+++
@@ -1,8 +0,0 @@
-# A template for an actual, intelligent agent
-# Receives an observation (features and RGB) and produces an action
-class RandomAgent():
-    def __init__(self, action_space):
-        self.action_space = action_space
-
-    def step(self, obs):
-        return self.action_space.sample()
