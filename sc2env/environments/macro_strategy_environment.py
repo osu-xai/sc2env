61bcc0f5e03b2d2a20332ed167a0b5c39492209b
Lawrence Neal <nealla@lwneal.com>
2018-09-18 11:45:02 -0700

    Update MacroStrategy

--- sc2env/environments/macro_strategy_environment.py
+++ sc2env/environments/macro_strategy_environment.py
@@ -2,18 +2,29 @@ import time
 import os
 import random
 import numpy as np
-from pysc2_util import register_map, unpack_timestep
+
+import imutil
+from pysc2_util import register_map
 from pysc2.env import sc2_env
 from pysc2.lib import actions
 
+
+# These parameters are adjustable without changing the .SC2Map
 MAP_NAME = 'MacroStrategy'
 MAP_SIZE = 64
 RGB_SCREEN_WIDTH = 400
 RGB_SCREEN_HEIGHT = 240
 
-# See the ResolveBattle trigger in the .SC2Map
-MAX_STEPS = 10
 
+# These parameters are based on the .SC2Map triggers
+# Open the map in the Galaxy editor for details
+MAX_STEPS = 10
+unit_id_to_name = {
+    1922: "CustomUnitCommandCenter",
+    1923: "CustomUnitPaper",
+    1924: "CustomUnitRock",
+    1925: "CustomUnitScissors",
+}
 action_to_ability_id = {
     1: 3771,
     2: 3773,
@@ -26,75 +37,125 @@ action_to_ability_id = {
 }
 action_to_name = {
     0: "No-Op",
-    1: "Paper 1",
-    2: "Rock 1",
-    3: "Scissors 1",
-    4: "Build SCV",
-    5: "Paper 2",
-    6: "Rock 2",
-    7: "Scissors 2",
-    8: "Scout",
+    1: "Paper (reserves)",
+    2: "Paper (front)",
+    3: "Rock (reserves)",
+    4: "Rock (front)",
+    5: "Scissors (reserves)",
+    6: "Scissors (front)",
+    7: "Scout",
+    8: "Counterintelligence",
 }
 
-class MacroStrategyEnvironment():
-    def __init__(self):
-        self.sc2env = make_sc2env()
+
+class MacroStrategyMultiplayerEnvironment():
+    """
+    This environment pits two agents against each other in a game of
+    incomplete information. Each agent trades off between building more
+    units, and gathering information about the enemy's units.
+    """
+    def __init__(self, render=False, video_filename=None, verbose=False, num_players=2):
+        if video_filename:
+            render = True
+        self.render = render
+        self.num_players = num_players
+        self.sc2env = make_sc2env(num_players, render=render)
+        self.video = None
+        if video_filename:
+            self.video = imutil.VideoMaker(filename=video_filename)
+        self.verbose = verbose
 
     def reset(self):
         self.step_sc2env()
         self.steps = 0
-        state, reward, done, info = unpack_timestep(self.last_timestep)
+        state, reward, done, info = self.unpack_state()
         return state
 
     def action_space(self):
         from gym.spaces.discrete import Discrete
         return Discrete(len(action_to_name))
 
-    # Step: Take an action and play the game out 10 seconds
+    # Step: Take an action and play the game out for ~10 seconds
     def step(self, action_player1, action_player2=None):
-        # 0: first timestep, 1: any other, 2: last timestep
-        if self.sc2env._state == 2:
+        if self.game_over():
             print('Game is over, cannot take further actions')
             return
 
-        print('Taking action t={}'.format(self.steps))
+        if self.verbose:
+            print('Taking action_player1={}, action_player2={} at t={}'.format(
+                action_player1, action_player2, self.steps))
         self.steps += 1
 
         if self.steps >= MAX_STEPS:
-            print('Game has reached limit of {} actions: simulating endgame'.format(MAX_STEPS))
+            if self.verbose:
+                print('Game has reached limit of {} actions: simulating endgame'.format(MAX_STEPS))
             self.step_until_endgame()
         else:
             if action_player1 > 0:
                 player1_ability_id = action_to_ability_id[action_player1]
                 self.use_custom_ability(player1_ability_id, 1)
-            if action_player2 > 0:
+            if self.num_players > 1 and action_player2 > 0:
                 player2_ability_id = action_to_ability_id[action_player2]
                 self.use_custom_ability(player2_ability_id, 2)
             self.step_sc2env()
-        return unpack_timestep(self.last_timestep)
+        if self.video:
+            screenshot = self.unpack_state()[0][3]
+            for _ in range(10):
+                self.video.write_frame(screenshot)
+        return self.unpack_state()
 
+    # Convert the SC2Env timestep into a Gym-style tuple
+    def unpack_state(self):
+        obs = self.last_timestep.observation
+        feature_map = np.array(obs.feature_minimap)
+        feature_screen = np.array(obs.feature_screen)
+        rgb_map = None
+        rgb_screen = None
+        if self.render:
+            rgb_map = np.array(obs.rgb_minimap)
+            rgb_screen = np.array(obs.rgb_screen)
+        state = (feature_map, feature_screen, rgb_map, rgb_screen)
+
+        reward = 0
+        done = self.game_over()
+        if done:
+            reward = 1 if self.first_player_victory() else -1
+            print('Finishing game at step {}'.format(self.steps))
+        info = {}
+        return state, reward, done, info
+
+    def game_over(self):
+        # SC2Env Game States
+        # 0: first timestep, 1: other, 2: last timestep
+        return self.sc2env._state == 2
+
+    def first_player_victory(self):
+        return self.sc2env._obs[0].player_result[0].result != 2
 
     def step_sc2env(self):
-        print('step_sc2env() state={}'.format(self.sc2env._state))
+        if self.verbose:
+            print('step_sc2env() state={}'.format(self.sc2env._state))
         # Step forward to synchronize clients
         start_time = time.time()
-        self.sc2env._controllers[0].step(count=1)
-        self.sc2env._controllers[1].step(count=1)
-        self.sc2env._controllers[0].observe()
-        self.sc2env._controllers[1].observe()
+        for i in range(self.num_players):
+            self.sc2env._controllers[i].step(count=1)
+            self.sc2env._controllers[i].observe()
 
         noop = actions.FUNCTIONS.no_op()
-        action_list = [noop, noop]
-        self.last_timestep, enemy_timestep = self.sc2env.step(action_list)
-        print('SC2Env step took {:.02f} sec'.format(time.time() - start_time))
+        action_list = [noop] * self.num_players
+        timesteps = self.sc2env.step(action_list)
+        self.last_timestep = timesteps[0]
+        if self.verbose:
+            print('SC2Env step took {:.02f} sec'.format(time.time() - start_time))
 
     def step_until_endgame(self):
-        import imutil
-        self.sc2env._step_mul = 36
-        while self.sc2env._state != 2:
+        if self.video:
+            self.sc2env._step_mul = 3
+        while not self.game_over():
             self.step_sc2env()
-            imutil.show(unpack_timestep(self.last_timestep)[0][3])
-
+            if self.video:
+                screenshot = self.unpack_state()[0][3]
+                self.video.write_frame(screenshot)
 
     def use_custom_ability(self, ability_id, player_id=1):
         # Sends a command directly to the SC2 protobuf API
@@ -121,7 +182,8 @@ class MacroStrategyEnvironment():
 
         # Bypass pysc2 and send the proto directly
         client = self.sc2env._controllers[player_id - 1]._client
-        print('Calling client.send_req for player_id {}'.format(player_id))
+        if self.verbose:
+            print('Calling client.send_req for player_id {}'.format(player_id))
         if self.sc2env._state == 2:
             print('Game is over, cannot send action')
             return
@@ -130,9 +192,14 @@ class MacroStrategyEnvironment():
 
 # Create the low-level SC2Env object, which we wrap with
 #  a high level Gym-style environment
-def make_sc2env():
-    env_args = {
-        'agent_interface_format': sc2_env.AgentInterfaceFormat(
+def make_sc2env(num_players, render=False):
+    if num_players == 1:
+        players = [sc2_env.Agent(sc2_env.Race.terran)]
+    else:
+        players = [sc2_env.Agent(sc2_env.Race.terran), sc2_env.Agent(sc2_env.Race.terran)]
+
+    if render:
+        interface = sc2_env.AgentInterfaceFormat(
             feature_dimensions=sc2_env.Dimensions(
                 screen=(MAP_SIZE, MAP_SIZE),
                 minimap=(MAP_SIZE, MAP_SIZE)
@@ -141,13 +208,30 @@ def make_sc2env():
                 screen=(RGB_SCREEN_WIDTH, RGB_SCREEN_HEIGHT),
                 minimap=(RGB_SCREEN_WIDTH, RGB_SCREEN_HEIGHT),
             ),
-            action_space=actions.ActionSpace.FEATURES,
-        ),
+            action_space=actions.ActionSpace.FEATURES)
+    else:
+        interface = sc2_env.AgentInterfaceFormat(
+            feature_dimensions=sc2_env.Dimensions(
+                screen=(MAP_SIZE, MAP_SIZE),
+                minimap=(MAP_SIZE, MAP_SIZE)
+            ), action_space=actions.ActionSpace.FEATURES)
+
+    env_args = {
+        'agent_interface_format': interface,
         'map_name': MAP_NAME,
-        'step_mul': 170,  # 17 is ~1 action per second
-        'players': [sc2_env.Agent(sc2_env.Race.terran), sc2_env.Agent(sc2_env.Race.terran)],
+        'step_mul': 85,  # 17 is ~1 action per second
+        'players': players,
     }
     maps_dir = os.path.join(os.path.dirname(__file__), '..', 'maps')
-    register_map(maps_dir, env_args['map_name'], players=2)
+    register_map(maps_dir, env_args['map_name'], players=num_players)
     return sc2_env.SC2Env(**env_args)
+
+
+class MacroStrategyEnvironment(MacroStrategyMultiplayerEnvironment):
+    """
+    The single-player version, against a scripted opponent
+    """
+    def __init__(self, *args, **kwargs):
+        kwargs['num_players'] = 1
+        super().__init__(*args, **kwargs)
 
--- sc2env/play_macro_strategy.py
+++ sc2env/play_macro_strategy.py
@@ -1,54 +1,80 @@
-import json
-import random
-import numpy as np
-import os
-from tqdm import tqdm
+import sys
 import imutil
+import time
+import numpy as np
 
-import pysc2_util
 from random_agent import RandomAgent
-from environments import macro_strategy_environment
-from environments.macro_strategy_environment import MacroStrategyEnvironment, action_to_name
+from environments import macro_strategy
 
 
-def main():
-    # This environment involves choosing to build the right units
-    env = MacroStrategyEnvironment()
-    blue_agent = RandomAgent(env.action_space())
-    red_agent = RandomAgent(env.action_space())
+"""
+Macro Strategy environment
+In this environment, each agent builds up units to attack the enemy
+At each time step an agent may take one of the following actions:
+    - Immediately produce units
+    - Produce units linearly over time
+    - Invest in resource gathering to produce more units per time step
+    - Sabotage the opponent's resource gathering units
+The game continues until one side or the other wins.
+"""
+def main(render=False):
+    unique_id = int(time.time())
+    if render:
+        video_filename = 'video_{}.mp4'.format(unique_id)
+        env = macro_strategy.MacroStrategyEnvironment(video_filename=video_filename, verbose=True)
+    else:
+        env = macro_strategy.MacroStrategyEnvironment(render=False, verbose=True)
+
     state = env.reset()
+    done = False
 
-    for i in range(100):
-        # The buildable units are named Rock, Paper, and Scissors
-        # 1: Build paper in back
-        # 2: Build paper in front
-        # 3: Build rock in back
-        # 4: Build rock in front
-        # 5: Build scissors in back
-        # 6: Build scissors in front
-        # 7: Scout to reveal the enemy's army
-        blue_action = blue_agent.step()
-        red_action = red_agent.step()
+    agent = RandomAgent(env.action_space())
 
-        # Take the action and simulate the game for one time step (~10 seconds)
-        state, reward, done, info = env.step(my_action, enemy_action)
+    while not done:
+        # Our agent takes the state as input and selects actions to maximize reward
+        action = agent.step(state)
+
+        # Take an action and simulate the game for one time step (~5 seconds)
+        state, reward, done, info = env.step(action)
 
         # The state is a tuple of:
         #   features_minimap: np array of features from the minimap view
         #   features_screen: np array of features from the camera view
-        #   rgb_minimap: np array of an RGB pixel view of the minimap
-        #   rgb_screen: np array of RGB pixel rendered frame of Starcraft II
+        #   rgb_minimap: np array of an RGB pixel view of the minimap (if render=True)
+        #   rgb_screen: np array of RGB pixel rendered frame of Starcraft II (if render=True)
         features_minimap, features_screen, rgb_minimap, rgb_screen = state
 
-        top = imutil.show(rgb_minimap, resize_to=(400,240), return_pixels=True, display=False)
-        bottom = imutil.show(rgb_screen, resize_to=(400,240), return_pixels=True, display=False)
-        filename = "output_frame_{:05d}.jpg".format(i)
-        caption = 't={}  Left: {}  Right: {}'.format(
-            env.steps, action_to_name[my_action], action_to_name[enemy_action])
-        imutil.show(np.concatenate([top, bottom], axis=0), filename=filename, caption=caption)
-        if done:
-            print('Game is done')
-            break
+        # Example code for visualizing the state
+        filename = "output_frame_{}_{:05d}.jpg".format(unique_id, env.steps)
+        caption = macro_strategy.action_to_name[action]
+        caption = 't={}  Reward={}  Action: {}'.format(env.steps, reward, caption)
+
+        left = imutil.show(colorize(features_minimap[4]), resize_to=(256, 256), return_pixels=True, display=False, save=False)
+        right = imutil.show(colorize(features_minimap[5], mode='nipy'), resize_to=(256, 256), return_pixels=True, display=False, save=False)
+        pixels = np.concatenate([left, right], axis=1)
+        if render:
+            screenshot = imutil.show(rgb_screen, resize_to=(512, 288), return_pixels=True, display=False, save=False)
+            pixels = np.concatenate([pixels, screenshot], axis=0)
+        imutil.show(pixels, filename=filename, caption=caption)
+
+    print('Finished game with final reward {}'.format(reward))
+
+
+def colorize(pixels, mode='gnuplot'):
+    from matplotlib import cm
+    values = np.unique(pixels)
+    vmin, vmax = min(values), max(values)
+    new_pixels = np.zeros(pixels.shape + (3,))
+    if mode == 'gnuplot':
+        map = cm.gnuplot
+    else:
+        map = cm.nipy_spectral
+    for v in values:
+        val = (v - vmin) / (vmax - vmin)
+        new_pixels[np.where(pixels == v)] = map(val)[:3]
+    return new_pixels
+
 
 if __name__ == '__main__':
-    main()
+    render = '--render' in sys.argv
+    main(render=render)
