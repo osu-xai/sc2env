ffac90a64e64c2486ea9d4d2a589f61a77ec422b
Lawrence Neal <nealla@compute-gpu1.hpc.engr.oregonstate.edu>
2018-10-09 16:16:26 -0700

    Add install script

---
+++ install_starcraft2.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+confirm() {
+    # call with a prompt string or use a default
+    echo
+    read -r -p "${1:-Do you wish to continue? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+yellow() {
+	printf "\033[0;33m$*\033[0m\n"
+}
+
+
+echo 
+echo 
+yellow "StarCraftII Installer"
+if [ -f ~/StarCraftII ]; then
+    echo "StarCraftII is already installed in ~/StarCraftII"
+    echo "Do you wish to re-install StarCraftII?"
+    confirm || exit
+else
+    echo "StarCraftII is not installed"
+    echo "Installer will download StarCraftII to `pwd`/StarCraftII"
+    echo "Installer will add symbolic link $HOME/StarCraftII --> `pwd`/StarCraftII"
+    confirm || exit
+fi
+
+if [ ! -z "$SC2_CACHE" ]; then
+    echo "Copying pre-downloaded StarCraft files from  $SC2_CACHE ..."
+    cp -v $SC2_CACHE/*.zip .
+fi
+
+yellow "Performing StarCraftII installation..."
+
+wget -nc http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.0.2.zip
+unzip -P iagreetotheeula SC2.4.0.2.zip
+wget -nc https://github.com/deepmind/pysc2/releases/download/v1.2/mini_games.zip
+
+wget -nc http://blzdistsc2-a.akamaihd.net/MapPacks/Melee.zip
+wget -nc http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season3.zip
+wget -nc http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season2.zip
+wget -nc http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season1.zip
+
+unzip mini_games.zip -d StarCraftII/Maps/
+unzip -P iagreetotheeula Melee.zip -d StarCraftII/Maps/
+unzip -P iagreetotheeula Ladder2017Season3.zip -d StarCraftII/Maps/
+unzip -P iagreetotheeula Ladder2017Season2.zip -d StarCraftII/Maps/
+unzip -P iagreetotheeula Ladder2017Season1.zip -d StarCraftII/Maps/
+
+ln -s $(pwd)/StarCraftII $HOME/StarCraftII
+
+yellow "StarCraftII is installed to `pwd`/StarCraftII"
