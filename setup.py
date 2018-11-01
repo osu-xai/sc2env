4124d47a48db1b28a1d17a242f11efe17bf8c777
Lawrence Neal <nealla@lwneal.com>
2018-11-01 12:29:41 -0700

    Fix missing import

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sc2env',
-    version='0.1.3',
+    version='0.1.4',
     description='Simple StarCraftII AI environments',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
