299806bfd312780c611e7707af0983bceb686a1e
Lawrence Neal <nealla@lwneal.com>
2018-11-01 14:17:13 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sc2env',
-    version='0.1.5',
+    version='0.1.6',
     description='Simple StarCraftII AI environments',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
