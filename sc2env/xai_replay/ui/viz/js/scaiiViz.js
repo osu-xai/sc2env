db5bd43505cdeef03538296390cff7a9944f0cf9
jedirv <jedirv@gmail.com>
2019-01-14 11:25:50 -0800

    more first pass pruning

--- sc2env/xai_replay/ui/viz/js/scaiiViz.js
+++ sc2env/xai_replay/ui/viz/js/scaiiViz.js
@@ -1,42 +1,10 @@
 
-
-goog.require('proto.scaii.common.Action');
-goog.require('proto.scaii.common.AgentCfg');
-goog.require('proto.scaii.common.AgentEndpoint');
-goog.require('proto.scaii.common.AgentSupported');
-goog.require('proto.scaii.common.BackendCfg');
-goog.require('proto.scaii.common.BackendEndpoint');
-goog.require('proto.scaii.common.BackendInit');
-goog.require('proto.scaii.common.BackendSupported');
-goog.require('proto.scaii.common.BackendSupported.SerializationSupport');
-goog.require('proto.scaii.common.Cfg');
-goog.require('proto.scaii.common.Color');
-goog.require('proto.scaii.common.CoreCfg');
-goog.require('proto.scaii.common.CoreEndpoint');
-goog.require('proto.scaii.common.Endpoint');
-goog.require('proto.scaii.common.Entity');
-goog.require('proto.scaii.common.Error');
+//
+// SC2_CHANGE - do we need these?  If not, can ditch google lib.  If so, why did missing ones not cause problems
+//
 goog.require('proto.scaii.common.ExplanationPoint');
-goog.require('proto.scaii.common.InitAs');
-goog.require('proto.scaii.common.ModuleCfg');
-goog.require('proto.scaii.common.ModuleEndpoint');
-goog.require('proto.scaii.common.ModuleInit');
-goog.require('proto.scaii.common.ModuleSupported');
 goog.require('proto.scaii.common.MultiMessage');
-goog.require('proto.scaii.common.Other');
-goog.require('proto.scaii.common.PluginType');
-goog.require('proto.scaii.common.Pos');
-goog.require('proto.scaii.common.Rect');
 goog.require('proto.scaii.common.ScaiiPacket');
-goog.require('proto.scaii.common.SerializationFormat');
-goog.require('proto.scaii.common.SerializationRequest');
-goog.require('proto.scaii.common.SerializationResponse');
-goog.require('proto.scaii.common.Shape');
-goog.require('proto.scaii.common.State');
-goog.require('proto.scaii.common.SupportedBehavior');
-goog.require('proto.scaii.common.Triangle');
-goog.require('proto.scaii.common.Viz');
-goog.require('proto.scaii.common.VizInit');
 
 /**
 * Copyright (c) 2017-present, Oregon State University, Inc.
