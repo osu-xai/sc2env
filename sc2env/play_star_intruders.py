75ca54a78c81c71c26a875c0be37d95e0900ef53
Lawrence Neal <nealla@lwneal.com>
2019-05-21 18:36:20 -0700

    Fix StarIntruders reward scale
    Improve resolution and action choice for demo video output

--- sc2env/play_star_intruders.py
+++ sc2env/play_star_intruders.py
@@ -2,24 +2,27 @@ import sc2env
 import gym
 import imutil
 import time
+import numpy as np
 from sc2env.environments.star_intruders import StarIntrudersEnvironment
 
 
 def generate_demo_video():
-    env = StarIntrudersEnvironment()
+    env = StarIntrudersEnvironment(screen_size=512)
     state = env.reset()
     done = False
     print('Playing sample game with environment {}'.format(env))
     filename = 'game-StarIntruders-{}.mp4'.format(int(time.time()))
-    vid = imutil.Video(filename, framerate=4)
+    vid = imutil.Video(filename, framerate=8)
 
     rewards = []
+    action = env.action_space.sample()
     for t in range(200):
         if done:
             print('Finished episode with {} total reward after {} timesteps'.format(sum(rewards), len(rewards)))
             rewards = []
             state = env.reset()
-        action = env.action_space.sample()
+        if np.random.random() < .2:
+            action = env.action_space.sample()
         state, reward, done, info = env.step(action)
         minimap_ftr, screen_ftr, minimap_rgb, screen_rgb = state
         caption = 't={} Reward: {:.2f}'.format(t, reward)
