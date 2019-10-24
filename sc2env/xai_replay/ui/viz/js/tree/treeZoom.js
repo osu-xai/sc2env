719645b2b21663e9bb2cfab2fb98697520dc4dfb
Jed Irvine <jedirv@gmail.com>
2019-10-24 14:08:02 -0700

    forgot to push treeZoom.js

---
+++ sc2env/xai_replay/ui/viz/js/tree/treeZoom.js
@@ -0,0 +1,7 @@
+
+var alertWithZoomAndPanValuesOfModelBasedView = false;
+// Jed's laptop
+var treeZoom = 0.07881728856471155;
+var treePanX = 1050.1482910362288;
+var treePanY = -181.08463439628667;
+
