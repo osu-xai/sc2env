8e376a6a1a73c1c39bebadd616f4b197e62d1dfd
Jed Irvine <jedirv@gmail.com>
2019-12-04 15:19:20 -0800

    basic electron files that get system working sans server

---
+++ sc2env/xai_replay/ui/viz/main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+// to keep window from being garbage collected by javascript reference it:
+let win;
+
+//then…
+function createWindow(){
+	win = new BrowserWindow({width:1800, height:1000});
+
+    win.loadFile("index.html");
+    win.webContents.openDevTools();
+
+    win.on('closed', ()=> {
+        win = null;
+    });
+
+}
+
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin'){
+        app.quit();
+    }
+});
